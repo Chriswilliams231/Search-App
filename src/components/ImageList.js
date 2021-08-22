ace26675bfc1ab27e4ad2e1bc4d8e48d65fee105
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2021-08-22 14:56:23 -0700

    added ImageCard

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,15 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
+
 
 const ImageList = (props) => {
     console.log(props.images)
-    const images = props.images.map((id, urls, description) => {
-        return <img key={id} src={urls.regular} alt={description} />
+    const images = props.images.map((image) => {
+        // image={image} is passed as a prop down to ImageCard
+        return <ImageCard key={image.id} image={image} />
     })
-    return <div>{images}</div>;
+    return <div className="image-list">{images}</div>;
 }
 
 export default ImageList
